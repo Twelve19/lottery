29ef97c751062588a602e298e010a3807cc31004
lw <liwei>
2023-10-31 17:35:54 +0800

    保存开奖结果信息

--- src/main/java/com/lottery/mapper/result/DrawResultMapper.java
+++ src/main/java/com/lottery/mapper/result/DrawResultMapper.java
@@ -1,9 +1,22 @@
 package com.lottery.mapper.result;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lottery.framework.model.result.DrawResultDO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface DrawResultMapper extends BaseMapper<DrawResultDO> {
+
+    /**
+     * 根据期号获取结果
+     * @param issue 期号
+     * @return 结果
+     */
+    default DrawResultDO selectDrawResult(String issue) {
+        LambdaQueryWrapper<DrawResultDO> wrapper = new LambdaQueryWrapper<DrawResultDO>();
+        wrapper.eq(DrawResultDO::getIssue, issue);
+        return this.selectOne(wrapper);
+    }
 }
--- src/main/java/com/lottery/service/result/impl/DrawResultServiceImpl.java
+++ src/main/java/com/lottery/service/result/impl/DrawResultServiceImpl.java
@@ -1,5 +1,6 @@
 package com.lottery.service.result.impl;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSONObject;
@@ -8,11 +9,16 @@ import com.lottery.framework.model.result.DrawResultDO;
 import com.lottery.mapper.result.DrawResultMapper;
 import com.lottery.service.result.DrawResultService;
 import com.lottery.service.result.dto.DrawResultDTO;
+import com.lottery.service.result.dto.value.pool.PoolDrawDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @Slf4j
@@ -26,12 +32,41 @@ public class DrawResultServiceImpl extends ServiceImpl<DrawResultMapper, DrawRes
 
     @Override
     public boolean push() {
-        log.info("================拉取数据开始================");
-        String result = HttpUtil.get(url);
-        log.info(result);
-        log.info("================拉取数据结束================");
-        DrawResultDTO drawResultDTO = JSONObject.parseObject(result, DrawResultDTO.class);
-        System.out.println("drawResultDTO = " + drawResultDTO);
-        return false;
+        // 请求api接口
+        String resultJson = HttpUtil.get(url);
+        // json转换对象
+        DrawResultDTO drawResultDTO = JSONObject.parseObject(resultJson, DrawResultDTO.class);
+        List<PoolDrawDTO> poolDrawDTOS = drawResultDTO.getValue().getList();
+        for (PoolDrawDTO poolDrawDTO : poolDrawDTOS) {
+
+            // 保存结果
+            DrawResultDO drawResultDO = this.getDrawResultDO(poolDrawDTO);
+            DrawResultDO dbDrawResultDO = drawResultMapper.selectDrawResult(poolDrawDTO.getLotteryDrawNum());
+            if(Objects.nonNull(dbDrawResultDO)) {
+                drawResultDO.setId(dbDrawResultDO.getId());
+                drawResultMapper.updateById(drawResultDO);
+            }else {
+                drawResultMapper.insert(drawResultDO);
+            }
+
+        }
+
+        return true;
+    }
+
+    private DrawResultDO getDrawResultDO(PoolDrawDTO poolDrawDTO) {
+        String result = poolDrawDTO.getLotteryDrawResult();
+        String[] resultArr = result.split(StrUtil.SPACE);
+        DrawResultDO drawResultDO = new DrawResultDO();
+        drawResultDO.setR1(resultArr[0]);
+        drawResultDO.setR2(resultArr[1]);
+        drawResultDO.setR3(resultArr[2]);
+        drawResultDO.setR4(resultArr[3]);
+        drawResultDO.setR5(resultArr[4]);
+        drawResultDO.setB1(resultArr[5]);
+        drawResultDO.setB2(resultArr[6]);
+        drawResultDO.setIssue(poolDrawDTO.getLotteryDrawNum());
+        drawResultDO.setDrawTime(poolDrawDTO.getLotteryDrawTime());
+        return drawResultDO;
     }
 }
